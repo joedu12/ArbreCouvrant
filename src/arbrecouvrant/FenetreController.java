b7ded54ed7ff1c9ec19be76ae2aa7eb6a13771b4
joedu12 <joevin12@hotmail.fr>
2018-01-23 23:38:20 +0100

    JAVADOC et distance entre deux sommets

--- src/arbrecouvrant/FenetreController.java
+++ src/arbrecouvrant/FenetreController.java
@@ -7,6 +7,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+/**
+ * Controleur de la vue FXML
+ */
 public class FenetreController implements Initializable {
    @FXML
    private Graphe graphe;
@@ -14,6 +17,11 @@ public class FenetreController implements Initializable {
    @FXML
    private void toutEffacer(ActionEvent event)  {
 	   graphe.toutEffacer();
+   }
+   
+   @FXML
+   private void calculerDistance(ActionEvent event) {
+	   graphe.calculDistance();
    }
    
    @Override
