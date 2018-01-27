135b26061c5eabfcffea0e7f07e2c0388e912551
joedu12 <joevin12@hotmail.fr>
2018-01-27 02:20:18 +0100

    Keep it simple, stupid

--- src/arbrecouvrant/Graphe.java
+++ src/arbrecouvrant/Graphe.java
@@ -2,50 +2,110 @@ package arbrecouvrant;
 
 import java.util.ArrayList;
 
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 /*
  * Classe qui contiendra l'ensemble des points et arêtes
  */
-public class Graphe extends StackPane {
-	   private GraphicsContext calque;
-	   private ArrayList<Sommet> sommets = new ArrayList<>();
+public class Graphe extends Pane {
+	   private ArrayList<Sommet> listSommet = new ArrayList<>();
+	   private ArrayList<Text> listText = new ArrayList<>();
+	   public static boolean isDragging;
+	   MouseGestures mg = new MouseGestures();
 	   
-	    /**
-	     * Ajoute un sommet à chaque clic de la souris
-	     */
-	   public Graphe() {  
-		      Canvas dessin = new Canvas(1920, 1024);
-		      calque = dessin.getGraphicsContext2D();  
-		      Sommet.setCalque(calque);
-		      setOnMouseClicked(evt -> { tracerSommet(evt.getX(), evt.getY()); } );
-		      getChildren().add(dessin);
+		/**
+		 * Ajoute un sommet à chaque clic de la souris
+		 */
+	   public Graphe() {
+		   // évènement du Pane qui crée le sommet
+		   setOnMouseClicked(evt -> {
+			   if(!isDragging) {
+				   tracerSommet(evt.getX(), evt.getY());
+			   }
+		   } );
 	   }
 
 	   /*
 	    * Efface le l'ensemble du graphe via un évènement onAction sur un bouton dans le FXML
 	    */
-	   public void toutEffacer() { 
-	      calque.clearRect(0, 0, getWidth(), getHeight()); 
-	      sommets.clear();
+	   public void toutEffacer() {
+		  getChildren().clear(); 
+	      listSommet.clear();
 	   }
 	   
 	   /*
 	    * Calcule la distance entre les deux premiers sommets via un évènement onAction sur un bouton dans le FXML
 	    */
 	   public void calculDistance() {
-		   System.out.println("Distance : " + sommets.get(0).getDistance(sommets.get(1)));
+		   System.out.println("Distance : " + listSommet.get(0).getDistance(listSommet.get(1)));
 	   }
 
 	   /*
 	    * Dessine un cercle dans lequel on a son numéro
 	    */
 	   public void tracerSommet(double x, double y) {
-	      Sommet sommet = new Sommet(x, y);
-	      sommets.add(sommet);
-	      sommet.setNom(""+sommets.size());
-	      sommet.affiche();
+		      Sommet sommet = new Sommet(x, y);
+		      listSommet.add(sommet);
+		      getChildren().addAll(sommet);	      
+		      
+		      Text text = new Text(x-5, y+3,""+listSommet.size());
+		      text.setFill(Color.RED);
+		      listText.add(text);
+		      getChildren().addAll(text);
+		      
+		      // TODO: lier text et sommet dans un overlay
+		      
+		      mg.makeDraggable(sommet);
 	   }
+	   
+	    public static class MouseGestures {
+
+	        double orgSceneX, orgSceneY; // position d'origine
+	        double orgTranslateX, orgTranslateY; // position après drag'n'drop
+
+	        public void makeDraggable(Node node) {
+	            node.setOnMousePressed(sommetOnMousePressed);
+	            node.setOnMouseDragged(sommetOnMouseDragged);
+	        }
+
+	        EventHandler<MouseEvent> sommetOnMousePressed = new EventHandler<MouseEvent>() {
+	            @Override
+	            public void handle(MouseEvent evt) {
+	                orgSceneX = evt.getSceneX();
+	                orgSceneY = evt.getSceneY();
+
+	                Sommet s = ((Sommet) (evt.getSource()));
+	                orgTranslateX = s.getCenterX();
+	                orgTranslateY = s.getCenterY();
+	            }
+	        };
+
+	        EventHandler<MouseEvent> sommetOnMouseDragged = new EventHandler<MouseEvent>() {
+	            @Override
+	            public void handle(MouseEvent evt) {
+	                double offsetX = evt.getSceneX() - orgSceneX;
+	                double offsetY = evt.getSceneY() - orgSceneY;
+
+	                double newTranslateX = orgTranslateX + offsetX;
+	                double newTranslateY = orgTranslateY + offsetY;
+
+	                Sommet s = ((Sommet) (evt.getSource()));
+	                s.setCenterX(newTranslateX);
+	                s.setCenterY(newTranslateY);
+	                
+	                if(evt.getSource() instanceof Sommet) {
+	                	Graphe.isDragging = true;
+	                }
+	                else {
+	                	Graphe.isDragging = false;
+	                }
+	            }
+	        };
+
+	    }
 }
--- src/arbrecouvrant/Sommet.java
+++ src/arbrecouvrant/Sommet.java
@@ -1,42 +1,23 @@
 package arbrecouvrant;
 
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 /*
- * Classe qui modélise un sommet représenté par un cercle de rayon 20px dans le Graphe
+ * On réutilise la classe Circle :
+ * - Les muttateurs et accesseurs des coordonnées sont
+ * setCenter et getCenter.
+ * - Un cercle se déplace avec la méthode relocate.
  */
-public class Sommet {
-	private double x, y;
-	private String nom;
-	private final double rayon=20;
-	private static GraphicsContext calque;
-	
+public class Sommet extends Circle {
+
 	public Sommet(double x, double y) {
-		this.x = x;
-		this.y = y;
+		super(x, y, 20, new Color(1, 0, 0, 0.1));
 	}
 
-	public static void setCalque(GraphicsContext contexte) {
-		calque = contexte;
-		calque.setStroke(Color.RED);
-	}
-	
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	
-	public double getX() { return x; }
-	public double getY() { return y; }
-
-	public int getDistance(Sommet autre) {
-       int dX = (int) Math.abs(this.x - autre.getX());
-       int dY = (int) Math.abs(this.y - autre.getY());
-       return (int) Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
-	}
-      
-	public void affiche() {
-		calque.strokeOval(x-rayon, y-rayon, 2*rayon, 2*rayon);
-		calque.fillText(nom, x-6, y+3);
+	public double getDistance(Sommet autre) {
+       double dX = Math.abs(this.getCenterX() - autre.getCenterX());
+       double dY = Math.abs(this.getCenterY() - autre.getCenterY());
+       return Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
 	}
 }
