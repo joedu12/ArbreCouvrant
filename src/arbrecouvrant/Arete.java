99f1e3292708390991a9148f99dde727fc352305
joevin <joevin.soulenq@gmail.com>
2018-02-19 22:16:49 +0100

    Nettoyage du code

--- src/arbrecouvrant/Arete.java
+++ src/arbrecouvrant/Arete.java
@@ -5,15 +5,13 @@ import javafx.scene.shape.Line;
 
 import java.io.Serializable;
 
-/**
- * On utilise ici la classe Line
+/*
+ * On réutilise la classe Line
  */
 public class Arete extends Line implements Comparable<Arete>, Serializable {
-    private Sommet precedent;
-    private Sommet suivant;
+    private Sommet precedent, suivant;
     private double poids;
-    private boolean marque;
-    private boolean erreur;
+    private boolean marque, erreur;
 
     public Arete(Sommet a, Sommet b) {
         super(a.getX(), a.getY(), b.getX(), b.getY());
@@ -26,23 +24,18 @@ public class Arete extends Line implements Comparable<Arete>, Serializable {
 
         calculerPoids();
     }
+    public double getPoids() {
+        return poids;
+    }
+    public Sommet getPrecedent() { return precedent; }
+    public Sommet getSuivant()   { return suivant; }
+    public boolean isMarque() { return marque; }
+    public boolean isErreur() { return erreur; }
 
     private void calculerPoids() {
         double dX = Math.abs(precedent.getX() - suivant.getX());
         double dY = Math.abs(precedent.getY() - suivant.getY());
         poids = Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
-    }
-
-    public double getPoids() {
-        return poids;
-    }
-
-    public Sommet getPrecedent() { return precedent; }
-    public Sommet getSuivant()   { return suivant; }
-
-    @Override
-    public int compareTo(Arete a2) {
-        return Double.compare(this.getPoids(), a2.getPoids());
     }
 
     public void setMarque(boolean marque) {
@@ -54,16 +47,18 @@ public class Arete extends Line implements Comparable<Arete>, Serializable {
         }
     }
 
-    public boolean isMarque() { return marque; }
-
-    public boolean isErreur() { return erreur; }
-
     public void setErreur(boolean erreur) {
+        this.erreur = erreur;
         if(erreur) {
             setStroke(new Color(0, 0, 1, 0.2));
         } else {
             setStroke(new Color(1, 0, 0, 0.2));
         }
+    }
+
+    @Override
+    public int compareTo(Arete a2) {
+        return Double.compare(this.getPoids(), a2.getPoids());
     }
 
     @Override
--- src/arbrecouvrant/Sommet.java
+++ src/arbrecouvrant/Sommet.java
@@ -9,10 +9,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 
 /*
- * On réutilise la classe Circle :
- * - Les muttateurs et accesseurs des coordonnées sont
- * setCenter et getCenter.
- * - Un cercle se déplace avec la méthode relocate.
+ * On réutilise la classe Circle et Text
  */
 public class Sommet extends Group implements Serializable {
 	private double x, y;
@@ -34,52 +31,27 @@ public class Sommet extends Group implements Serializable {
         marque = false;
     }
 
-    public void setX(double valeur) {
-		this.x = valeur;
-		setTranslateX(valeur);
-	}
-	
-	public void setY (double valeur) {
-		this.y = valeur;
-		setTranslateY(valeur);
-	}
-
+    public void setX(double valeur) { this.x = valeur; }
+	public void setY (double valeur) { this.y = valeur; }
     public void setNom(String valeur) {
         texte.setText(valeur);
     }
-
     public void setMarque(boolean marque) {
-	    this.marque = marque;
-	    texte.setFill(Color.GREEN);
+		this.marque = marque;
+		texte.setFill(Color.GREEN);
 	}
+	public void setListeCycle(HashSet<String> listeCycle) { this.listeCycle = listeCycle; }
 	
-	public double getX() {
-		return x;
-	}
-	
-	public double getY() {
-		return y;
-	}
-	
-	public String getNom() {
-		return nom;
-	}
-
+	public double getX() { return x; }
+	public double getY() { return y; }
+	public String getNom() { return nom; }
     public boolean isMarque() { return marque; }
+	public HashSet<String> getListeCycle() { return listeCycle; }
 
-    public boolean equals(Sommet sommet) {
-        if(x == sommet.getX() && y == sommet.getY()) { return true; }
-        else { return false; }
-    }
-
-	public HashSet<String> getListeCycle() {
-		return listeCycle;
+	public boolean equals(Sommet sommet) {
+		if(x == sommet.getX() && y == sommet.getY()) { return true; }
+		else { return false; }
 	}
-
-	public void setListeCycle(HashSet<String> listeCycle) {
-		this.listeCycle = listeCycle;
-	}
-
 	@Override
 	public String toString() {
 		String statut = (marque) ? "marqué" : "non-marqué";
